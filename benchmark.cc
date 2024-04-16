d1b17598d7dc9e000434abb4a1ee76d1aac32382
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-04-16 15:15:24 +0000

    add dynamic

--- benchmark.cc
+++ benchmark.cc
@@ -161,12 +161,89 @@ void benchmark_simulation_openmp()
     std::cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";
 }
 
+void benchmark_simulation_openmp_dy()
+{ // Finite Volume simulation
+
+    // Simulation parameters
+    double v0 = 1.0;  // Initial velocity
+    double eta = 0.5; // random fluctuation in angle (in radians)
+    double L = 10;    // size of the box
+    double R = 1;     // interaction radius
+    double dt = 0.2;  // time step
+
+    // Initialise
+    std::default_random_engine generator;
+    generator.seed(static_cast<unsigned int>(17));
+    std::uniform_real_distribution<double> uniform(0, 1);
+
+    // Initialize bird position
+    std::vector<double> x(N);
+    std::vector<double> y(N);
+    for (int i = 0; i < N; ++i)
+    {
+        x[i] = uniform(generator) * L;
+        y[i] = uniform(generator) * L;
+    }
+
+    // Initialize bird velocity
+    std::vector<double> theta(N);
+    std::vector<double> vx(N);
+    std::vector<double> vy(N);
+    for (int i = 0; i < N; ++i)
+    {
+        theta[i] = 2 * M_PI * uniform(generator);
+        vx[i] = v0 * cos(theta[i]);
+        vy[i] = v0 * sin(theta[i]);
+    }
+
+    // start time counter
+    auto start = std::chrono::high_resolution_clock::now();
+
+    // Simulation
+    for (int i = 0; i < Nt; i++)
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            // Update position
+            x[i] += vx[i] * dt;
+            y[i] += vy[i] * dt;
+
+            // Apply periodic boundary conditions
+            x[i] = fmod(x[i], L);
+            y[i] = fmod(y[i], L);
+            if (x[i] < 0)
+                x[i] += L; // Handle negative values
+            if (y[i] < 0)
+                y[i] += L; // Handle negative values
+        }
+
+        theta = simulation_openmp_dy(x, y, theta, R);
+
+        // Update velocities
+        for (int i = 0; i < N; ++i)
+        {
+            // Update theta randomly
+            theta[i] += eta * (uniform(generator) - 0.5);
+            // Update velocity components
+            vx[i] = v0 * cos(theta[i]);
+            vy[i] = v0 * sin(theta[i]);
+        }
+    }
+    // stop time counter
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout << "Openmp dynamic optimzation: " << std::endl;
+    std::cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";
+}
+
 int main()
 {
 
     std::cout << "Running benchmarks..." << std::endl;
     benchmark_simulation();
     benchmark_simulation_openmp();
+    benchmark_simulation_openmp_dy();
     std::cout
         << "All tests passed!" << std::endl;
 }
--- simulation.h
+++ simulation.h
@@ -6,4 +6,6 @@
 
 std::vector<double> simulation(const std::vector<double> &x, const std::vector<double> &y, std::vector<double> theta, const double R);
 std::vector<double> simulation_openmp(const std::vector<double> &x, const std::vector<double> &y, std::vector<double> theta, const double R);
+std::vector<double> simulation_openmp_dy(const std::vector<double> &x, const std::vector<double> &y, std::vector<double> theta, const double R);
+
 #endif
