2f21f3410db65622f212d9b055e569ba98d7fd3e
Gustaf Larsson <gustaf.larsson01@gmail.com>
2024-05-06 13:53:29 +0200

    hej

--- simulation.cc
+++ simulation.cc
@@ -110,10 +110,12 @@ std::vector<double> simulation_mpi(const std::vector<double> &x, const std::vect
 
     int rank, size, provided;
 
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+    //MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);  //Get number of processes
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); //Get each process rank
+
+    cout << size;
 
     int N = x.size();
     std::vector<double> mean_theta(N, 0.0);
