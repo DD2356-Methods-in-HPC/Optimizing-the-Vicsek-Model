3dd37d7bad3321c060834c190a569e44ac905d70
Gustaf Larsson <gustaf.larsson01@gmail.com>
2024-05-06 13:29:57 +0200

    updated

--- simulation.h
+++ simulation.h
@@ -3,7 +3,7 @@
 #include <vector>
 #include <cmath>
 #include <omp.h>
-//#include <mpi.h>
+#include <mpi.h>
 
 std::vector<double> simulation(const std::vector<double> &x, const std::vector<double> &y, std::vector<double> theta, const double R);
 std::vector<double> simulation_openmp(const std::vector<double> &x, const std::vector<double> &y, std::vector<double> theta, const double R);
