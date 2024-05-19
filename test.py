d6aa2bd6310738dc2f65f3c3dfb2e38f9d155906
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-05-19 19:46:57 +0200

    new test case

--- test.py
+++ test.py
@@ -10,16 +10,16 @@ Simulate Viscek model for flocking birds
 
 
 def main():
-    """ Finite Volume simulation """
+    """Finite Volume simulation"""
 
     # Simulation parameters
-    R = 1        # interaction radius
-    Nt = 1      # number of time steps
+    R = 1  # interaction radius
+    Nt = 1  # number of time steps
     # number of birds
 
-    x = np.array([1.0, 0.0, 1.0, 1.0])
+    x = np.array([0.0, 10.0, 100.0, 200.0])
     #  y = {1, 1.0, 0.0, 0.3};
-    y = np.array([1.0, 1.0, 0.0, 0.3])
+    y = np.array([0.0, 10.0, 100.0, 2000.0])
 
     N = len(x)  # number of birds
     # bird velocities
@@ -30,20 +30,20 @@ def main():
     mean_theta = theta
     for b in range(N):
         #        print((x-x[b])**2+(y-y[b])**2)
-        neighbors = (x-x[b])**2+(y-y[b])**2 < R**2
+        neighbors = (x - x[b]) ** 2 + (y - y[b]) ** 2 < R**2
         sx = np.sum(np.cos(theta[neighbors]))
         # print(np.cos(theta[neighbors]))
         sy = np.sum(np.sin(theta[neighbors]))
-        print("=====")
-        print(sx)
-        print(sy)
-        print(np.arctan2(sy, sx))
+        # print("=====")
+        # print(sx)
+        # print(sy)
+        # print(np.arctan2(sy, sx))
         mean_theta[b] = np.arctan2(sy, sx)
     #    print(mean_theta)
 
-        # add random perturbations
+    # add random perturbations
     theta = mean_theta
-    # print(theta)
+    print(theta)
 
     return 0
 
