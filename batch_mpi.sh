d03a0ad7e1cefcae96dbe65c589007ca34bdca61
Elliot <elliot.darth@gmail.com>
2024-05-25 18:44:32 +0200

    added basic batch scripts to project repo

---
+++ batch_mpi.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash -l
+# The -l above is required to get the full environment with modules
+
+# Second job allocation
+#SBATCH -J vicsekJob_MPI
+#SBATCH -t 1:00:00
+#SBATCH -A edu24.DD2356
+#SBATCH -p main 
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+#SBATCH -e error_file_MPI.e
+
+# Function to run the executable multiple times
+run_executable() {
+    local mode=$1
+    local num_iterations=$2
+    local output_file=$3
+
+    for i in $(seq 1 $num_iterations); do
+        echo "Run $i - Mode: $mode" >> $output_file
+        srun simulation $mode >> $output_file
+        echo "" >> $output_file     # add blank line
+    done
+}
+
+# Run with mode "MPI"
+run_executable "MPI" 5 "output_MPI.txt"
+
