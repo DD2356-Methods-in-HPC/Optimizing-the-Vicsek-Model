37b85815b39ef8142ff6196e4778824ae09d9d93
Gustaf Larsson <gustaf.larsson01@gmail.com>
2024-05-28 15:08:43 +0200

    benchmark.cc

--- benchmark.cc
+++ benchmark.cc
@@ -11,8 +11,8 @@
 
 #include "simulation.h" 
 
-const int Nt = 5; // number of time steps
-const int N = 10;  // number of birds
+const int Nt = 500; // number of time steps
+const int N = 10000;  // number of birds
 
 /**
  * @brief Perform benchmark simulations using a specified simulation function.
